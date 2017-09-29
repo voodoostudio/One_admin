370040cdbaf3a4083d813ed33cf68ba31073f5ca
IC <himac!>
2017-09-29 18:35:15 +0300

    Started modifying object view page

--- resources/views/bread/read.blade.php
+++ resources/views/bread/read.blade.php
@@ -1,4 +1,4 @@
-@extends('voyager::master')
+@extends('voyager::master_metronic')
 
 @section('page_title','View '.$dataType->display_name_singular)
 
@@ -21,7 +21,7 @@
 @stop
 
 @section('content')
-    <div class="page-content read container-fluid">
+    <div class="page-content read container-fluid" style="display: none">
         <div class="row">
             <div class="col-md-12">
 
@@ -112,6 +112,268 @@
 
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="m-grid__item m-grid__item--fluid m-wrapper">
+        <!-- BEGIN: Subheader -->
+        <div class="m-subheader ">
+            <div class="d-flex align-items-center">
+                <div class="mr-auto">
+                    <h3 class="m-subheader__title m-subheader__title--separator">
+                        Object view page
+                    </h3>
+                    <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+                        <li class="m-nav__item m-nav__item--home">
+                            <a href="#" class="m-nav__link m-nav__link--icon">
+                                <i class="m-nav__link-icon la la-home"></i>
+                            </a>
+                        </li>
+                        <li class="m-nav__separator">
+                            -
+                        </li>
+                        <li class="m-nav__item">
+                            <a href="" class="m-nav__link">
+                                <span class="m-nav__link-text">
+                                    Objects
+                                </span>
+                            </a>
+                        </li>
+                        <li class="m-nav__separator">
+                            -
+                        </li>
+                        <li class="m-nav__item">
+                            <a href="" class="m-nav__link">
+                                <span class="m-nav__link-text">
+                                    Object ID
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="m-dropdown m-dropdown--inline m-dropdown--arrow m-dropdown--align-right m-dropdown--align-push" data-dropdown-toggle="hover" aria-expanded="true">
+                        <a href="#" class="m-portlet__nav-link btn btn-lg btn-secondary  m-btn m-btn--outline-2x m-btn--air m-btn--icon m-btn--icon-only m-btn--pill  m-dropdown__toggle">
+                            <i class="la la-plus m--hide"></i>
+                            <i class="la la-ellipsis-h"></i>
+                        </a>
+                        <div class="m-dropdown__wrapper">
+                            <span class="m-dropdown__arrow m-dropdown__arrow--right m-dropdown__arrow--adjust"></span>
+                            <div class="m-dropdown__inner">
+                                <div class="m-dropdown__body">
+                                    <div class="m-dropdown__content">
+                                        <ul class="m-nav">
+                                            <li class="m-nav__section m-nav__section--first m--hide">
+															<span class="m-nav__section-text">
+																Quick Actions
+															</span>
+                                            </li>
+                                            <li class="m-nav__item">
+                                                <a href="" class="m-nav__link">
+                                                    <i class="m-nav__link-icon flaticon-share"></i>
+                                                    <span class="m-nav__link-text">
+																	Activity
+																</span>
+                                                </a>
+                                            </li>
+                                            <li class="m-nav__item">
+                                                <a href="" class="m-nav__link">
+                                                    <i class="m-nav__link-icon flaticon-chat-1"></i>
+                                                    <span class="m-nav__link-text">
+																	Messages
+																</span>
+                                                </a>
+                                            </li>
+                                            <li class="m-nav__item">
+                                                <a href="" class="m-nav__link">
+                                                    <i class="m-nav__link-icon flaticon-info"></i>
+                                                    <span class="m-nav__link-text">
+																	FAQ
+																</span>
+                                                </a>
+                                            </li>
+                                            <li class="m-nav__item">
+                                                <a href="" class="m-nav__link">
+                                                    <i class="m-nav__link-icon flaticon-lifebuoy"></i>
+                                                    <span class="m-nav__link-text">
+																	Support
+																</span>
+                                                </a>
+                                            </li>
+                                            <li class="m-nav__separator m-nav__separator--fit"></li>
+                                            <li class="m-nav__item">
+                                                <a href="#" class="btn btn-outline-danger m-btn m-btn--pill m-btn--wide btn-sm">
+                                                    Submit
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END: Subheader -->
+        <div class="m-content">
+            <!--Begin::Main Portlet-->
+            <div class="row">
+                <div class="col-xl-8">
+                    <!--begin:: Widgets/Blog-->
+                    <div class="m-portlet m-portlet--full-height gallery_container">
+                        <div class="m-portlet__body">
+                            <div class="m-widget19">
+                                <div class="m-widget19__pic m-portlet-fit--sides" style1="height: 280px">
+                                    <img src="/assets/metronic_5/theme/dist/html/default/assets/app/media/img//blog/blog1.jpg" alt="">
+                                    <h3 class="m-widget19__title m--font-light">
+                                        Here will be gallery
+                                    </h3>
+                                    <div class="m-widget19__shadow"></div>
+                                </div>
+                                <div class="m-widget19__content">
+                                    <div class="m-widget19__header">
+                                        <div class="m-widget19__user-img">
+                                            <img class="m-widget19__img" src="/assets/metronic_5/theme/dist/html/default/assets/app/media/img//users/user1.jpg" alt="">
+                                        </div>
+                                        <div class="m-widget19__info">
+                                            <span class="m-widget19__username">
+                                                Anna Krox
+                                            </span>
+                                            <br>
+                                            <span class="m-widget19__time">
+                                                Admin, who posted this object
+                                            </span>
+                                        </div>
+                                        <div class="m-widget19__stats object_price">
+                                            <span class="m-widget19__number m--font-brand">
+                                               CHF 180.000
+                                            </span>
+                                            <span class="m-widget19__comment">
+                                                Price comments
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div class="m-widget19__body">
+                                        This is description text: Lorem Ipsum is simply dummy text of the printing and typesetting industry scrambled it to make text of the printing and typesetting industry scrambled a type specimen book text of the dummy text of the printing printing and typesetting industry scrambled dummy text of the printing.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end:: Widgets/Blog-->
+                </div>
+                <div class="col-xl-4">
+                    <!--begin:: Widgets/New Users-->
+                    <div class="m-portlet m-portlet--full-height">
+                        <div class="m-portlet__head">
+                            <div class="m-portlet__head-caption">
+                                <div class="m-portlet__head-title">
+                                    <h3 class="m-portlet__head-text">
+                                        General
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="m-portlet__body">
+                            <!--begin::Widget 14-->
+                            <div class="m-widget4">
+                                <div class="row">
+                                    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-6">
+                                        <!--begin::Widget 14 Item-->
+                                        <div class="m-widget4__item">
+                                            <div class="m-widget4__info">
+                                                <span class="m-widget4__title">
+                                                    Parameter (dropdown)
+                                                </span>
+                                                <br>
+                                                <span class="m-widget4__sub">
+                                                    Value
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <!--end::Widget 14 Item-->
+                                    </div>
+                                    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-6">
+                                        <!--begin::Widget 14 Item-->
+                                        <div class="m-widget4__item">
+                                            <div class="m-widget4__info">
+                                                <span class="m-widget4__title">
+                                                    Parameter (dropdown)
+                                                </span>
+                                                <br>
+                                                <span class="m-widget4__sub">
+                                                    Value
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <!--end::Widget 14 Item-->
+                                    </div>
+                                    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-6">
+                                        <!--begin::Widget 14 Item-->
+                                        <div class="m-widget4__item">
+                                            <div class="m-widget4__info">
+                                                <span class="m-widget4__title">
+                                                    Parameter (dropdown)
+                                                </span>
+                                                <br>
+                                                <span class="m-widget4__sub">
+                                                    Value
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <!--end::Widget 14 Item-->
+                                    </div>
+                                    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-6">
+                                        <!--begin::Widget 14 Item-->
+                                        <div class="m-widget4__item">
+                                            <div class="m-widget4__info">
+                                                <span class="m-widget4__title">
+                                                    Parameter (dropdown)
+                                                </span>
+                                                <br>
+                                                <span class="m-widget4__sub">
+                                                    Value
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <!--end::Widget 14 Item-->
+                                    </div>
+                                    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-6">
+                                        <!--begin::Widget 14 Item-->
+                                        <div class="m-widget4__item">
+                                            <div class="m-widget4__info">
+                                                <span class="m-widget4__title">
+                                                    Parameter (dropdown)
+                                                </span>
+                                                <br>
+                                                <span class="m-widget4__sub">
+                                                    Value
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <!--end::Widget 14 Item-->
+                                    </div>
+                                    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-6">
+                                        <!--begin::Widget 14 Item-->
+                                        <div class="m-widget4__item">
+                                            <div class="m-widget4__info">
+                                                <span class="m-widget4__title">
+                                                    Parameter (checkbox)
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <!--end::Widget 14 Item-->
+                                    </div>
+                                </div>
+                            </div>
+                            <!--end::Widget 14-->
+                        </div>
+                    </div>
+                    <!--end:: Widgets/New Users-->
+                </div>
+            </div>
+            <!--End::Main Portlet-->
         </div>
     </div>
 @stop
