eb8cf4b15e85e45d4fea6899a0421759c0847438
Alex <a.loscinin@voodoo-studio.ch>
2017-11-01 21:19:43 +0200

    edit multiple image upload

--- resources/views/bread/read.blade.php
+++ resources/views/bread/read.blade.php
@@ -231,9 +231,11 @@
                             <div class="m-widget19">
                                 <div class="m-widget19__pic m-portlet-fit--sides" style1="height: 280px">
                                     <div class="object_gallery">
-                                        @foreach(json_decode($dataTypeContent->image) as $image)
-                                            <div><img src="{{ URL::to('storage') }}/{{ $image }}" alt=""></div>
-                                        @endforeach
+                                        @if(!empty(json_decode($dataTypeContent->image)))
+                                            @foreach(json_decode($dataTypeContent->image) as $image)
+                                                <div><img src="{{ URL::to('storage') }}/{{ $image }}" alt=""></div>
+                                            @endforeach
+                                        @endif
                                     </div>
                                     <h3 class="m-widget19__title m--font-light">
                                         {{ $dataTypeContent->title_fr }}
