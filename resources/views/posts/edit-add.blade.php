263672b6397f24d59ba952e100300c130ae69486
IC <himac!>
2017-11-27 16:46:07 +0200

    hide toolboxes

--- resources/views/posts/edit-add.blade.php
+++ resources/views/posts/edit-add.blade.php
@@ -97,7 +97,7 @@
                         </li>
                     </ul>
                 </div>
-                <div>
+                <div style="display: none;">
                     <div class="m-dropdown m-dropdown--inline m-dropdown--arrow m-dropdown--align-right m-dropdown--align-push" data-dropdown-toggle="hover" aria-expanded="true">
                         <a href="#" class="m-portlet__nav-link btn btn-lg btn-secondary  m-btn m-btn--outline-2x m-btn--air m-btn--icon m-btn--icon-only m-btn--pill  m-dropdown__toggle">
                             <i class="la la-plus m--hide"></i>
