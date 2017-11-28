fdb244226d04d494024788dc41712dfbbd89a585
vladimir <vcaraseni@gmail.com>
2017-11-28 11:07:31 +0200

    added new users of every role

--- publishable/database/seeds/UsersTableSeeder.php
+++ publishable/database/seeds/UsersTableSeeder.php
@@ -33,6 +33,36 @@ class UsersTableSeeder extends Seeder
                 'remember_token' => str_random(60),
                 'role_id'        => $super_admin_role->id,
             ]);
+
+            $admin_role = Role::where('name', 'admin')->firstOrFail();
+
+            User::create([
+                'name'           => 'Admin',
+                'email'          => 'admin@admin.com',
+                'password'       => bcrypt('admin'),
+                'remember_token' => str_random(60),
+                'role_id'        => $admin_role->id,
+            ]);
+
+            $moderator_role = Role::where('name', 'moderator')->firstOrFail();
+
+            User::create([
+                'name'           => 'Moderator',
+                'email'          => 'moderator@mod.com',
+                'password'       => bcrypt('moder'),
+                'remember_token' => str_random(60),
+                'role_id'        => $moderator_role->id,
+            ]);
+
+            $user_role = Role::where('name', 'user')->firstOrFail();
+
+            User::create([
+                'name'           => 'User',
+                'email'          => 'user@user.com',
+                'password'       => bcrypt('user'),
+                'remember_token' => str_random(60),
+                'role_id'        => $user_role->id,
+            ]);
         }
     }
 }
