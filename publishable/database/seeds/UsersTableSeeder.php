33c3f340a3ff71645b3bfe164f0b2a90569c7ec5
vova-pc <vcaraseni@gmail.com>
2017-11-28 17:56:51 +0200

    delete default users

--- publishable/database/seeds/UsersTableSeeder.php
+++ publishable/database/seeds/UsersTableSeeder.php
@@ -34,35 +34,35 @@ class UsersTableSeeder extends Seeder
                 'role_id'        => $super_admin_role->id,
             ]);
 
-            $admin_role = Role::where('name', 'admin')->firstOrFail();
-
-            User::create([
-                'name'           => 'Admin',
-                'email'          => 'admin@admin.com',
-                'password'       => bcrypt('admin'),
-                'remember_token' => str_random(60),
-                'role_id'        => $admin_role->id,
-            ]);
-
-            $moderator_role = Role::where('name', 'moderator')->firstOrFail();
-
-            User::create([
-                'name'           => 'Moderator',
-                'email'          => 'moderator@mod.com',
-                'password'       => bcrypt('moder'),
-                'remember_token' => str_random(60),
-                'role_id'        => $moderator_role->id,
-            ]);
-
-            $user_role = Role::where('name', 'user')->firstOrFail();
-
-            User::create([
-                'name'           => 'User',
-                'email'          => 'user@user.com',
-                'password'       => bcrypt('user'),
-                'remember_token' => str_random(60),
-                'role_id'        => $user_role->id,
-            ]);
+//            $admin_role = Role::where('name', 'admin')->firstOrFail();
+//
+//            User::create([
+//                'name'           => 'Admin',
+//                'email'          => 'admin@admin.com',
+//                'password'       => bcrypt('admin'),
+//                'remember_token' => str_random(60),
+//                'role_id'        => $admin_role->id,
+//            ]);
+//
+//            $moderator_role = Role::where('name', 'moderator')->firstOrFail();
+//
+//            User::create([
+//                'name'           => 'Moderator',
+//                'email'          => 'moderator@mod.com',
+//                'password'       => bcrypt('moder'),
+//                'remember_token' => str_random(60),
+//                'role_id'        => $moderator_role->id,
+//            ]);
+//
+//            $user_role = Role::where('name', 'user')->firstOrFail();
+//
+//            User::create([
+//                'name'           => 'User',
+//                'email'          => 'user@user.com',
+//                'password'       => bcrypt('user'),
+//                'remember_token' => str_random(60),
+//                'role_id'        => $user_role->id,
+//            ]);
         }
     }
 }
