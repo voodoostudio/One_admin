ae8a12e4ddcc6846b667b33294a7fdbb8acd7dd0
IC <himac!>
2017-12-18 10:12:36 +0200

    Clients add page modified

--- resources/views/clients/edit-add.blade.php
+++ resources/views/clients/edit-add.blade.php
@@ -956,68 +956,6 @@
                             </li>
                         </ul>
                     </div>
-                    <div>
-                        <div class="m-dropdown m-dropdown--inline m-dropdown--arrow m-dropdown--align-right m-dropdown--align-push" data-dropdown-toggle="hover" aria-expanded="true">
-                            <a href="#" class="m-portlet__nav-link btn btn-lg btn-secondary  m-btn m-btn--outline-2x m-btn--air m-btn--icon m-btn--icon-only m-btn--pill  m-dropdown__toggle">
-                                <i class="la la-plus m--hide"></i>
-                                <i class="la la-ellipsis-h"></i>
-                            </a>
-                            <div class="m-dropdown__wrapper">
-                                <span class="m-dropdown__arrow m-dropdown__arrow--right m-dropdown__arrow--adjust"></span>
-                                <div class="m-dropdown__inner">
-                                    <div class="m-dropdown__body">
-                                        <div class="m-dropdown__content">
-                                            <ul class="m-nav">
-                                                <li class="m-nav__section m-nav__section--first m--hide">
-                                                <span class="m-nav__section-text">
-                                                    Quick Actions
-                                                </span>
-                                                </li>
-                                                <li class="m-nav__item">
-                                                    <a href="" class="m-nav__link">
-                                                        <i class="m-nav__link-icon flaticon-share"></i>
-                                                        <span class="m-nav__link-text">
-                                                        Activity
-                                                    </span>
-                                                    </a>
-                                                </li>
-                                                <li class="m-nav__item">
-                                                    <a href="" class="m-nav__link">
-                                                        <i class="m-nav__link-icon flaticon-chat-1"></i>
-                                                        <span class="m-nav__link-text">
-                                                        Messages
-                                                    </span>
-                                                    </a>
-                                                </li>
-                                                <li class="m-nav__item">
-                                                    <a href="" class="m-nav__link">
-                                                        <i class="m-nav__link-icon flaticon-info"></i>
-                                                        <span class="m-nav__link-text">
-                                                        FAQ
-                                                    </span>
-                                                    </a>
-                                                </li>
-                                                <li class="m-nav__item">
-                                                    <a href="" class="m-nav__link">
-                                                        <i class="m-nav__link-icon flaticon-lifebuoy"></i>
-                                                        <span class="m-nav__link-text">
-                                                        Support
-                                                    </span>
-                                                    </a>
-                                                </li>
-                                                <li class="m-nav__separator m-nav__separator--fit"></li>
-                                                <li class="m-nav__item">
-                                                    <a href="#" class="btn btn-outline-danger m-btn m-btn--pill m-btn--wide btn-sm">
-                                                        Submit
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
 
@@ -1048,33 +986,33 @@
                                     </div>
                                     <div class="m-portlet__body">
                                         <div class="form-group m-form__group row">
-                                            <div class="col-lg-3 margin_bottom_10">
+                                            <div class="col-lg-3 col-md-6 margin_bottom_10">
                                                 <label>Name</label>
                                                 <input type="text" id="" class="form-control m-input" placeholder="Name" value="" name="name">
                                             </div>
-                                            <div class="col-lg-3 margin_bottom_10">
+                                            <div class="col-lg-3 col-md-6 margin_bottom_10">
                                                 <label>Second Name</label>
                                                 <input type="text" id="" class="form-control m-input" placeholder="Second Name" value="" name="last_name">
                                             </div>
-                                            <div class="col-lg-3 margin_bottom_10">
+                                            <div class="col-lg-3 col-md-6 margin_bottom_10">
                                                 <label for="lng_corres">Langue de correspondance</label>
-                                                <select name="lng_corres" id="lng_corres">
+                                                <select class="form-control m-select2 custom_select2 elem-categories" id="lng_corres" name="lng_corres" data-placeholder="Select Floor">
                                                     @foreach(TCG\Voyager\Models\UserLanguage::all() as $lng)
                                                         <option value="{{ $lng->reference }}">{{ $lng->value }}</option>
                                                     @endforeach
                                                 </select>
                                             </div>
-                                            <div class="col-lg-3 margin_bottom_10">
+                                            <div class="col-lg-3 col-md-6 margin_bottom_10">
                                                 <label>Email</label>
                                                 <input type="text" id="" class="form-control m-input" placeholder="Email" value="" name="email">
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-lg-12 m--align-right">
-                                        <button type="submit" class="btn btn-primary btn-lg">Enregistrer</button>
-                                    </div>
                                 </div>
                                 <!--end::Portlet-->
+                            </div>
+                            <div class="col-lg-12 m--align-right">
+                                <button type="submit" class="btn btn-primary btn-lg">Enregistrer</button>
                             </div>
                         </div>
                         <!-- End Adresse -->
