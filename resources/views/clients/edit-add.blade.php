9b5b1722a64cf67c759770290af45fb22b416647
IC <himac!>
2018-01-03 11:52:39 +0300

    removed breadcrumbs

--- resources/views/clients/edit-add.blade.php
+++ resources/views/clients/edit-add.blade.php
@@ -892,43 +892,6 @@
         <!--end::Modal-->
     @else
         <div class="m-grid__item m-grid__item--fluid m-wrapper" style="">
-            <!-- BEGIN: Subheader -->
-            <div class="m-subheader ">
-                <div class="d-flex align-items-center">
-                    <div class="mr-auto">
-                        <h3 class="m-subheader__title m-subheader__title--separator">
-                            Add/Edit new object
-                        </h3>
-                        <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
-                            <li class="m-nav__item m-nav__item--home">
-                                <a href="#" class="m-nav__link m-nav__link--icon">
-                                    <i class="m-nav__link-icon la la-home"></i>
-                                </a>
-                            </li>
-                            <li class="m-nav__separator">
-                                -
-                            </li>
-                            <li class="m-nav__item">
-                                <a href="" class="m-nav__link">
-                                <span class="m-nav__link-text">
-                                    Clients
-                                </span>
-                                </a>
-                            </li>
-                            <li class="m-nav__separator">
-                                -
-                            </li>
-                            <li class="m-nav__item">
-                                <a href="" class="m-nav__link">
-                                <span class="m-nav__link-text">
-                                    Create client
-                                </span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
 
             <!-- END: Subheader -->
             <div class="m-content">
