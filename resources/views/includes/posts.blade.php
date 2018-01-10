758d5e8b22b8dd339b2dba854023f8343e457690
IC <himac!>
2018-01-10 11:28:41 +0200

    Subheader hidden

--- resources/views/includes/posts.blade.php
+++ resources/views/includes/posts.blade.php
@@ -1,36 +1,13 @@
 {{--{{ dd($dataTypeContent->toArray()) }}--}}
-<div class="m-subheader ">
-    <div class="d-flex align-items-center">
-        <div class="mr-auto">
-            <h3 class="m-subheader__title m-subheader__title--separator">
-                Object view page
-            </h3>
-            <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
-                <li class="m-nav__item m-nav__item--home">
-                    <a href="#" class="m-nav__link m-nav__link--icon">
-                        <i class="m-nav__link-icon la la-home"></i>
-                    </a>
-                </li>
-                <li class="m-nav__separator">-</li>
-                <li class="m-nav__item">
-                    <a href="" class="m-nav__link">
-                        <span class="m-nav__link-text">
-                            Objects
-                        </span>
-                    </a>
-                </li>
-                <li class="m-nav__separator">-</li>
-                <li class="m-nav__item">
-                    <a href="" class="m-nav__link">
-                        <span class="m-nav__link-text">
-                            Object ID
-                        </span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
+{{--<div class="m-subheader ">--}}
+    {{--<div class="d-flex align-items-center">--}}
+        {{--<div class="mr-auto">--}}
+            {{--<h3 class="m-subheader__title m-subheader__title--separator">--}}
+                {{--Bien immobilier --}}
+            {{--</h3>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
 <div class="m-content">
     <!--Begin::Main Portlet-->
     <div class="row">
